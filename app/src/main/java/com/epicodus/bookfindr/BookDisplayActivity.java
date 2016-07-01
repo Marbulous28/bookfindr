76fd5769748967ddd517865cc1df015711f1f324
Peter Beach <peterbeach2@gmail.com>
2016-07-01 09:45:18 -0700

    added ability for user to input a genre they are interested in.

--- app/src/main/java/com/epicodus/bookfindr/BookDisplayActivity.java
+++ app/src/main/java/com/epicodus/bookfindr/BookDisplayActivity.java
@@ -1,13 +1,20 @@
 package com.epicodus.bookfindr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BookDisplayActivity extends AppCompatActivity {
+    private TextView mGenreInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_display);
+        mGenreInfo = (TextView) findViewById(R.id.genreInfo);
+        Intent intent = getIntent();
+        String genre = intent.getStringExtra("genre");
+        mGenreInfo.setText("You selected " + genre + " as your genre.");
     }
 }
--- app/src/main/java/com/epicodus/bookfindr/MainActivity.java
+++ app/src/main/java/com/epicodus/bookfindr/MainActivity.java
@@ -5,20 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button mFindBooksButton;
+    private EditText mGenreInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mGenreInput = (EditText) findViewById(R.id.genreInput);
         mFindBooksButton = (Button) findViewById(R.id.findBooksButton);
         mFindBooksButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
+                String genre = mGenreInput.getText().toString();
                 Intent intent = new Intent(MainActivity.this, BookDisplayActivity.class);
+                intent.putExtra("genre", genre);
                 startActivity(intent);
             }
         });
