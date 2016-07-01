9b9dc47e670e73e75ad71fcfde07d26a51923acc
Peter Beach <peterbeach2@gmail.com>
2016-07-01 09:22:42 -0700

    added button that takes the user to a new activity where books are displayed.

--- app/src/main/java/com/epicodus/bookfindr/MainActivity.java
+++ app/src/main/java/com/epicodus/bookfindr/MainActivity.java
@@ -1,13 +1,26 @@
 package com.epicodus.bookfindr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindBooksButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindBooksButton = (Button) findViewById(R.id.findBooksButton);
+        mFindBooksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BookDisplayActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
