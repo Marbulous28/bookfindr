85f578512c6f4699c62762ff42cf3540e5a2653a
Peter Beach <peterbeach2@gmail.com>
2016-07-08 11:43:20 -0700

    added Constants.java with base URL for api calls. Added BookService.java which creates the url string for the API Call.

---
+++ app/src/main/java/com/epicodus/bookfindr/BookService.java
@@ -0,0 +1,33 @@
+package com.epicodus.bookfindr;
+
+
+import java.util.ArrayList;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by Peter on 7/8/16.
+ */
+public class BookService {
+
+    public static void findBooks(String subject, String keyword, Callback callback) {
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .build();
+
+        String url = Constants.GOOGLE_BASE_URL + keyword + "+subject:" + subject +"&key=" + Constants.GOOGLE_BOOKS_KEY;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+
+    }
+
+}
---
+++ app/src/main/java/com/epicodus/bookfindr/Constants.java
@@ -0,0 +1,9 @@
+package com.epicodus.bookfindr;
+
+/**
+ * Created by Peter on 7/8/16.
+ */
+public class Constants {
+    public static final String GOOGLE_BOOKS_KEY = BuildConfig.GOOGLE_BOOKS_KEY;
+    public static final String GOOGLE_BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+}
