6426ccc6195e05a9b6bf53112d95fd2b31fc4ae8
Peter Beach <peterbeach2@gmail.com>
2016-07-13 23:32:36 -0700

    added intent that takes the user from fragment to amazon if they want to order a book.

--- app/src/main/java/com/epicodus/bookfindr/ui/BookDetailFragment.java
+++ app/src/main/java/com/epicodus/bookfindr/ui/BookDetailFragment.java
@@ -1,6 +1,8 @@
 package com.epicodus.bookfindr.ui;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -17,9 +19,10 @@ import org.parceler.Parcels;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class BookDetailFragment extends Fragment {
+public class BookDetailFragment extends Fragment implements View.OnClickListener {
     @Bind(R.id.ratingTextView) TextView mRatingLabel;
     @Bind(R.id.bookTitleTextView) TextView mNameLabel;
+    @Bind(R.id.websiteTextView) TextView mWebsiteLabel;
 
     private Book mBook;
 
@@ -41,11 +44,21 @@ public class BookDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_book_detail, container, false);
         ButterKnife.bind(this, view);
+        mWebsiteLabel.setOnClickListener(this);
 
 
         mNameLabel.setText(mBook.getTitle());
         mRatingLabel.setText(mBook.getRating());
 
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mWebsiteLabel) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.amazon.com/"));
+            startActivity(webIntent);
+        }
     }
 }
